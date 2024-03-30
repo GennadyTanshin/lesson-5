741faee9662a03e007f5284e81cdf2e435d76c83
GennadyTanshin <163913220+GennadyTanshin@users.noreply.github.com>
2024-03-30 18:45:02 +0300

    add new class

---
+++ main.py
@@ -0,0 +1,26 @@
+class Vorior():
+    def __init__(self, name, power, endurance, hair_color):
+        self.name = name
+        self.power = power
+        self.endurance = endurance
+        self.hair_color = hair_color
+    def sleep(self):
+        print(f"{self.name} спит!")
+        self.endurance += 2
+
+    def eat(self):
+        print(f"{self.name} сел кушать")
+        self.power += 1
+
+    def hit(self):
+        print(f"{self.name} бьет когото")
+        self.endurance -= 6
+
+    def walk(self):
+        print(f"{self.name} гуляет")
+
+    def info(self):
+        print(f" Имя воина {self.name}")
+        print(f" Сила воина {self.power}")
+        print(f" Выносливость воина {self.endurance}")
+        print(f" Цвет волос воина {self.hair_color}")
