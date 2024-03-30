3a609db875f9a88634dbae3cf10ddccda54b4dcf
GennadyTanshin <163913220+GennadyTanshin@users.noreply.github.com>
2024-03-30 20:25:01 +0300

    add new code in bank.py

---
+++ bank.py
@@ -0,0 +1,26 @@
+class Account():
+    def __init__(self, id, balance = 0):
+        self.id = id
+        self.balance = balance
+
+    def deposit(self, money):
+        if  money > 0:
+            self.balance += money
+            print(f"Bы успешно пополнили счет. Сумма на счете - {self.balance}")
+
+    def withdraw(self, money):
+        if money > self.balance:
+            print(f"Heдocтаточно средств на счете")
+        elif money <= self.balance:
+            self.balance -= money
+            print(f"Bы успешно сняли {money} рублей. Остаток на счете: {self.balance}")
+
+    def all_balance(self):
+        print(f"Текущий баланс - {self.balance}")
+
+man = Account("12323132", 600)
+
+man.all_balance()
+man.withdraw(300)
+man.withdraw(800)
+man.deposit(23000)
